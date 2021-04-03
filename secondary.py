0efbe9db0e1fd4bf2be6e1be0d79d0e7df613050
mzhao8 <>
2021-04-03 16:40:30 +0000

    Initial commit

---
+++ secondary.py
@@ -0,0 +1,26 @@
+import concurrent.futures
+
+MAX_THREADS = 30
+
+def download_url(url):
+  print(url)
+  resp = requests.get(url)
+  title =''.join(x for x in url if x.isalpha()) + "html"
+
+  with open(title, 'wb') as fh:
+    fh.write(resp.content)
+
+  time.sleep(.25)
+
+def download_stories(story_urls):
+  threads = min(MAX_THREADS, len(story_urls))
+
+  with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+    executor.map(download_url, story_urls)
+
+def main(story_urls):
+  t0 = time.time()
+  download_stories
+  t1 = time.time()
+  print(f"{t1 - t0} seconds to download {len(story_urls)} stories.")
+
